717b42dba5d0526daddbaab222b9636739ca5222
yasaichi <yasaichi@users.noreply.github.com>
2015-07-27 12:01:23 +0900

    change database from sqlite3 to postgresql

--- db/migrate/20150725110746_add_columns_to_oauth_access_tokens.rb
+++ db/migrate/20150725110746_add_columns_to_oauth_access_tokens.rb
@@ -1,9 +1,6 @@
 class AddColumnsToOauthAccessTokens < ActiveRecord::Migration
   def change
     add_column :oauth_access_tokens, :note, :string, after: :refresh_token
-    add_column :oauth_access_tokens, :updated_at, :datetime, after: :created_at
-
-    # for sqlite3
-    change_column :oauth_access_tokens, :updated_at, :datetime, null: false
+    add_column :oauth_access_tokens, :updated_at, :datetime, null: false, after: :created_at
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150725110746) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer  "resource_owner_id", null: false
     t.integer  "application_id",    null: false
@@ -24,7 +27,7 @@ ActiveRecord::Schema.define(version: 20150725110746) do
     t.string   "scopes"
   end
 
-  add_index "oauth_access_grants", ["token"], name: "index_oauth_access_grants_on_token", unique: true
+  add_index "oauth_access_grants", ["token"], name: "index_oauth_access_grants_on_token", unique: true, using: :btree
 
   create_table "oauth_access_tokens", force: :cascade do |t|
     t.integer  "resource_owner_id"
@@ -39,9 +42,9 @@ ActiveRecord::Schema.define(version: 20150725110746) do
     t.datetime "updated_at",        null: false
   end
 
-  add_index "oauth_access_tokens", ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true
-  add_index "oauth_access_tokens", ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id"
-  add_index "oauth_access_tokens", ["token"], name: "index_oauth_access_tokens_on_token", unique: true
+  add_index "oauth_access_tokens", ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true, using: :btree
+  add_index "oauth_access_tokens", ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id", using: :btree
+  add_index "oauth_access_tokens", ["token"], name: "index_oauth_access_tokens_on_token", unique: true, using: :btree
 
   create_table "oauth_applications", force: :cascade do |t|
     t.string   "name",                      null: false
@@ -53,7 +56,7 @@ ActiveRecord::Schema.define(version: 20150725110746) do
     t.datetime "updated_at"
   end
 
-  add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true
+  add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -70,7 +73,7 @@ ActiveRecord::Schema.define(version: 20150725110746) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
