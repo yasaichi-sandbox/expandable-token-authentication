a3793a22e7e164bee75cb43e4c955d189da9c0a8
yasaichi <yasaichi@users.noreply.github.com>
2015-07-27 11:00:14 +0900

    add db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create!(email: 'test@example.com', password: 'password')
+user.personal_access_tokens.create!(note: 'test')
